16b56044c7cbce5292092879aa2415f68b9527ad
Olivia Pomeranc <oliviapomeranc@hotmail.co.uk>
2020-08-25 12:19:04 +0100

    charity new form/controller/route/view

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,2 +1,21 @@
 class CharitiesController < ApplicationController
+  def new
+    @charity = Charity.new
+  end
+
+  def create
+    @charity = Charity.new(charity_params)
+    @charity.user = current_user
+    if @charity.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def charity_params
+    params.require(:charity).permit(:user_id, :name, :phone_number, :description, :website)
+  end
 end
