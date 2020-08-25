61f05e19b49ab30f278882a2b1a2752acdb29e1f
paul sipasseuth <paulsipasseuth@gmail.com>
2020-08-25 15:39:58 +0100

    adding validation

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,4 +3,9 @@ class Mission < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
   # has_many :experiences
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :address, presence: true
+  validates :number_of_volunteers, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :start_date, presence: true
 end
