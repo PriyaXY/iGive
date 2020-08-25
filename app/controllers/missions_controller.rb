58ba97a46a7133b5f97fc25e2a170dbcb17c52a6
paul sipasseuth <paulsipasseuth@gmail.com>
2020-08-25 14:06:52 +0100

    mission controlle create

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,2 +1,27 @@
 class MissionsController < ApplicationController
+
+  def new
+    @mission = Mission.new
+  end
+
+  def create
+    @mission = Mission.new(strong_params)
+    # @boat = Boat.find(params[:boat_id])
+    # @booking.boat = @boat
+    # @booking.user = current_user
+    @charity = Charity.find(params[:charity_id])
+    @mission.charity = @charity
+    @mission.user = current_user
+    if @mission.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:mission).permit(:start_date, :title, :description, :address, :number_of_volunteers)
+  end
 end
