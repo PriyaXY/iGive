68bfa00d838c3a09b42901d9d851c2ec8e064e23
Priya Biring <priya.biring@gmail.com>
2020-08-27 11:13:39 +0100

    geocoding things

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,11 +10,14 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
   }
-};
-
-export { initMapbox };
-
-
+  if (mapElement) {
+    // [ ... ]
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
 if (mapElement) {
   // [ ... ]
   const markers = JSON.parse(mapElement.dataset.markers);
@@ -24,3 +27,22 @@ if (mapElement) {
       .addTo(map);
   });
 }
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+if (mapElement) {
+  // [ ... ]
+  fitMapToMarkers(map, markers);
+}
+  }
+};
+
+
+
+export { initMapbox };
+
+
