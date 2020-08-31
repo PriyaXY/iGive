975c308c9bd649c6453c02735a9eb9f34929814a
Priya Biring <priya.biring@gmail.com>
2020-08-31 18:12:24 +0100

    chatroom stuff

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,5 +2,22 @@ class ChatroomsController < ApplicationController
   def show
    @chatroom = Chatroom.find(params[:id])
  end
-end
 
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    if @message.save
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+    else
+      render "chatrooms/show"
+    end
+  end
+
+  private
+
+  def message_params
+  params.require(:chatroom).permit(:name)
+  end
+end
