ccac2c3a2b4ac5c93a6a1a899aae918117b0cab9
Priya Biring <priya.biring@gmail.com>
2020-09-01 18:00:13 +0100

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_110132) do
+ActiveRecord::Schema.define(version: 2020_09_01_144355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,10 @@ ActiveRecord::Schema.define(version: 2020_09_01_110132) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.bigint "booking_id"
+    t.index ["booking_id"], name: "index_chatrooms_on_booking_id"
+    t.index ["user_id"], name: "index_chatrooms_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -107,6 +111,8 @@ ActiveRecord::Schema.define(version: 2020_09_01_110132) do
   add_foreign_key "bookings", "missions"
   add_foreign_key "bookings", "users"
   add_foreign_key "charities", "users"
+  add_foreign_key "chatrooms", "bookings"
+  add_foreign_key "chatrooms", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
   add_foreign_key "missions", "charities"
